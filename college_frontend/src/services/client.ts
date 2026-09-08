bf28bf691731c7499aa56478d01d116660c9f313
MSR <murlekr@gmail.com>
2026-09-08 11:18:59 +0530

    client.ts
    accesstoken and refreshtoken - to attach with all the apis etc.

--- college_frontend/src/services/client.ts
+++ college_frontend/src/services/client.ts
@@ -2,7 +2,11 @@ import axios from "axios"
 import { ENV } from "../config/environment"
 
 import type { InternalAxiosRequestConfig } from "axios"
-import { getAccessToken, saveToken, deleteTokens } from "./tokenServices"
+import { getAccessToken, saveToken, deleteTokens, getRefreshToken } from "./tokenServices"
+
+interface RetryableRequestConfig extends InternalAxiosRequestConfig{
+  _retry?: boolean
+}
 
 export const apiClient = axios.create({
   baseURL:ENV.API_BASE_URL,
@@ -11,3 +15,95 @@ export const apiClient = axios.create({
     "Content-Type":"application/json"
   }  
 })
+
+const refreshClient = axios.create({
+  baseURL:ENV.API_BASE_URL,
+  timeout:10000,
+  headers:{
+    "Content-Type":"application/json"
+  }
+})
+
+apiClient.interceptors.request.use(
+  async(config) => {
+    const accessToken = await getAccessToken()
+    if(accessToken){
+      config.headers.Authorization = `Bearer ${accessToken}`
+    }
+    return config
+  }
+)
+apiClient.interceptors.response.use(
+  (response) =>{
+    return response
+  },
+  async(error) =>{
+    const originalRequest = 
+    error.config as RetryableRequestConfig;
+    
+    if(error.response.status === 401 && !originalRequest._retry){
+      originalRequest._retry = true
+    }
+    try{
+      const refreshToken = await getRefreshToken()
+      if(!refreshToken){
+        await deleteTokens()
+        return Promise.reject(error)
+      }
+      const response = await refreshClient.post("api/auth/token/refresh/", 
+        {
+          refresh: refreshToken
+        });
+        const newAccessToken = response.data.access
+        const newRefreshToken = response.data.refresh ?? refreshToken;
+        await saveToken(
+          newAccessToken, 
+          newRefreshToken
+        );
+        originalRequest.headers.Authorization = `Bearer ${newAccessToken}`
+        return apiClient(originalRequest);
+      } catch(refreshError){
+        await deleteTokens()
+        return Promise.reject(
+          refreshError
+
+        );
+      }
+      return Promise.reject(error);
+    }   
+);
+
+// apiClient.interceptors.request.use(async (config:InternalAxiosRequestConfig)=>{
+//   const accessToken = await getAccessToken()
+//   if(accessToken){
+//     config.headers.Authorization = `Bearer ${accessToken}`
+//   } 
+//   return config
+// }, (error)=>{
+//   return Promise.reject(error)
+// } )
+
+// apiClient.interceptors.response.use((response)=>{
+//   return response
+// }, async (error)=>{
+//   const originalRequest = error.config
+//   if(error.response.status === 401 && !originalRequest._retry){
+//     originalRequest._retry = true
+//     try{{
+//       const refreshToken = await getRefreshToken()
+//       if(!refreshToken){
+//         deleteTokens()
+//         return Promise.reject(error)
+//       }
+//       const response = await refreshClient.post("/auth/refresh", {refreshToken})
+//       const {accessToken, refreshToken:newRefreshToken} = response.data
+//       saveToken(accessToken, newRefreshToken)
+//       originalRequest.headers.Authorization = `Bearer ${accessToken}`
+//       return apiClient(originalRequest)
+//     }catch(err){
+//       deleteTokens()
+//       return Promise.reject(err)
+//     }
+//   }
+//   return Promise.reject(error)
+// })
